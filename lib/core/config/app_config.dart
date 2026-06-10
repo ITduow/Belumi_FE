7a64ac31d8e9d5aae3360fb1a556448d99fe1fbb
huuanh20 <anhnhse180234@fpt.edu.vn>
2026-06-10 10:28:44 +0700

    config: update default API base URL to HTTPS production endpoint

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -3,7 +3,7 @@ import 'package:flutter/foundation.dart';
 class AppConfig {
   static const String apiBaseUrl = String.fromEnvironment(
     'BELUMI_API_BASE_URL',
-    defaultValue: 'http://belumi-api.ap-southeast-1.elasticbeanstalk.com/api',
+    defaultValue: 'https://api.belumi.site/api',
   );
 
   static String get resolvedApiBaseUrl {
